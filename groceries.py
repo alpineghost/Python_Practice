a40a314795705f5bb314ad8a965c4a9cde3f420e
Jonathan Geis <jonathangeis@gmail.com>
2017-07-10 20:03:50 -0400

    Print the number of products

--- groceries.py
+++ groceries.py
@@ -1,4 +1,5 @@
 import code
+
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
@@ -21,4 +22,13 @@ products = [
     {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ]
+
 print(products)
+
+print("-----------------")
+print("THERE ARE " + str(len(products)) + " PRODUCTS:")
+
+
+
+
+#code.interact(local=locals())
